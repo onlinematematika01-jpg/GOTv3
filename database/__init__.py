78cc78f2c15807b3ed1dc5237db0830d191cc54a
onlinematematika01-jpg <onlinematematika01@gmail.com>
2026-03-30 09:10:44 +0500

    Update __init__.py

--- database/__init__.py
+++ database/__init__.py
@@ -2,11 +2,13 @@ from .engine import create_tables, AsyncSessionFactory, get_session
 from .models import *
 from .repositories import (
     UserRepo, HouseRepo, WarRepo, AllianceRepo,
-    IronBankRepo, ChronicleRepo, MarketRepo
+    IronBankRepo, ChronicleRepo, MarketRepo, BotSettingsRepo,
+    HukmdorClaimRepo
 )
 
 __all__ = [
     "create_tables", "AsyncSessionFactory", "get_session",
     "UserRepo", "HouseRepo", "WarRepo", "AllianceRepo",
-    "IronBankRepo", "ChronicleRepo", "MarketRepo",
+    "IronBankRepo", "ChronicleRepo", "MarketRepo", "BotSettingsRepo",
+    "HukmdorClaimRepo",
 ]
