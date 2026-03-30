372a431282dea312f0d93cde3c58af09469affdf
onlinematematika01-jpg <onlinematematika01@gmail.com>
2026-03-30 17:48:37 +0500

    Update __init__.py

--- keyboards/__init__.py
+++ keyboards/__init__.py
@@ -2,12 +2,14 @@ from .keyboards import (
     main_menu_keyboard, war_menu_keyboard, market_keyboard,
     iron_bank_keyboard, diplomacy_keyboard, surrender_or_fight_keyboard,
     confirm_keyboard, admin_keyboard, house_list_keyboard, quantity_keyboard,
-    back_only_keyboard, with_back, back_button, rating_menu_keyboard
+    back_only_keyboard, with_back, back_button, rating_menu_keyboard,
+    alliance_request_keyboard
 )
 
 __all__ = [
     "main_menu_keyboard", "war_menu_keyboard", "market_keyboard",
     "iron_bank_keyboard", "diplomacy_keyboard", "surrender_or_fight_keyboard",
     "confirm_keyboard", "admin_keyboard", "house_list_keyboard", "quantity_keyboard",
-    "back_only_keyboard", "with_back", "back_button", "rating_menu_keyboard"
+    "back_only_keyboard", "with_back", "back_button", "rating_menu_keyboard",
+    "alliance_request_keyboard"
 ]
