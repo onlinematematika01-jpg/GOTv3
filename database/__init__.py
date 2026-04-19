7f1d49d831329ca5c4d35498c68fd21ee206737c
onlinematematika01-jpg <onlinematematika01@gmail.com>
2026-04-19 05:08:50 +0500

    Add WarDeploymentRepo and PrisonerRepo to exports

--- database/__init__.py
+++ database/__init__.py
@@ -3,12 +3,12 @@ from .models import *
 from .repositories import (
     UserRepo, HouseRepo, WarRepo, AllianceRepo,
     IronBankRepo, ChronicleRepo, MarketRepo, BotSettingsRepo,
-    HukmdorClaimRepo
+    HukmdorClaimRepo, WarDeploymentRepo, PrisonerRepo,
 )
 
 __all__ = [
     "create_tables", "AsyncSessionFactory", "get_session",
     "UserRepo", "HouseRepo", "WarRepo", "AllianceRepo",
     "IronBankRepo", "ChronicleRepo", "MarketRepo", "BotSettingsRepo",
-    "HukmdorClaimRepo",
+    "HukmdorClaimRepo", "WarDeploymentRepo", "PrisonerRepo",
 ]
