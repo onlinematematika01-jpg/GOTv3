c13650db160d9751dc8dc00a4fbce1078e86a7c6
onlinematematika01-jpg <onlinematematika01@gmail.com>
2026-04-19 05:59:16 +0500

    Update __init__.py

--- keyboards/__init__.py
+++ keyboards/__init__.py
@@ -5,6 +5,7 @@ from .keyboards import (
     back_only_keyboard, with_back, back_button, rating_menu_keyboard,
     alliance_request_keyboard, alliance_group_menu_keyboard, alliance_invite_keyboard,
     deploy_resources_keyboard, deploy_confirm_keyboard,
+    capture_lord_keyboard, prisoner_manage_keyboard, pay_ransom_keyboard,
 )
 
 __all__ = [
@@ -14,4 +15,5 @@ __all__ = [
     "back_only_keyboard", "with_back", "back_button", "rating_menu_keyboard",
     "alliance_request_keyboard", "alliance_group_menu_keyboard", "alliance_invite_keyboard",
     "deploy_resources_keyboard", "deploy_confirm_keyboard",
+    "capture_lord_keyboard", "prisoner_manage_keyboard", "pay_ransom_keyboard",
 ]
