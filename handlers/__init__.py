1df1c460b832f9eda52845baa659709cf21a205d
onlinematematika01-jpg <onlinematematika01@gmail.com>
2026-04-28 22:03:05 +0500

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -6,6 +6,7 @@ from handlers.war import router as war_router
 from handlers.bank import router as bank_router
 from handlers.diplomacy import router as diplomacy_router
 from handlers.chat import router as chat_router
+from handlers.admin import router as admin_router
 from handlers.chronicle import router as chronicle_router
 from handlers.claim import router as claim_router
 from handlers.rating import router as rating_router
@@ -29,6 +30,7 @@ def register_all_handlers(dp: Dispatcher):
     dp.include_router(bank_router)
     dp.include_router(diplomacy_router)
     dp.include_router(chat_router)
+    dp.include_router(admin_router)
     dp.include_router(chronicle_router)
     dp.include_router(claim_router)
     dp.include_router(rating_router)
